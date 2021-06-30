be8e083fccb7c3ff4fadbf941d4a17b3d61e403c
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-30 12:09:20 -0400

    create query service using events

--- basic-react-app/query_service/index.js
+++ basic-react-app/query_service/index.js
@@ -6,8 +6,31 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+const posts = {};
 
-app.post('/events', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'postCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'commentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(4002, () => console.log('listening on 4002'));
